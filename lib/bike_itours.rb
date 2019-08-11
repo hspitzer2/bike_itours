ac70e5e70eea7752db4bb5b22b2c5ac3096b32ed
Hillary Spitzer <hspitzer2@gmail.com>
2019-08-11 02:05:01 +0000

    Setup environment by adding require_relative and require statements

--- lib/bike_itours.rb
+++ lib/bike_itours.rb
@@ -1,5 +1,9 @@
-require "bike_itours/version"
-require "bike_itours/cli"
+require_relative "bike_itours/version"
+require_relative "bike_itours/cli"
+require_relative "bike_itours/scraper"
+
+require "nokogiri"
+require "open-uri"
 
 module BikeItours
   class Error < StandardError; end
--- lib/bike_itours/cli.rb
+++ lib/bike_itours/cli.rb
@@ -1,6 +1,11 @@
-class BikeItours::CLI 
-  # Which repo is origin?
-  def caller
+class BikeItours::CLI
+  def call
     puts "Benvenuti amici, ciao bella bicicletta!"
   end
+
+  def get_list
+  end
+
+  def get_cli_input
+  end 
 end
