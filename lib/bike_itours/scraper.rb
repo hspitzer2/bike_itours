30ecc2e4e39f3e198540646d48fb2412261a871c
hspitzer <hspitzer2@gmail.com>
2019-08-19 18:04:10 -0400

    add list tours and get details to scraper

---
+++ lib/bike_itours/scraper.rb
@@ -0,0 +1,32 @@
+require 'nokogiri'
+require 'open-uri'
+    
+class BikeItours::Scraper
+  def list_tours
+
+    site = "https://www.cyclebreaks.com/tours/italy/"
+
+    doc = Nokogiri::HTML(open(site))
+
+    names = doc.css(".summary-item-h h2").map(&:text)
+    list_names = names.map.with_index(1) do |name, index|
+      "#{index}. #{name}"
+    end
+    puts list_names
+  end
+
+  def get_details(tour)
+
+    site = "https://www.cyclebreaks.com/tours/italy/"
+
+    doc = Nokogiri::HTML(open(site))
+
+    links = doc.css("a.cta-box-btn").map{|link| link.attr("href")}
+
+    scrape_detail_link = Nokogiri::HTML(open(links[tour-1]))
+
+    highlights = scrape_detail_link.css("div.content-tour p").text
+
+    puts highlights
+  end
+end
